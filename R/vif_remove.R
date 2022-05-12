a7389cb0aeee7a92ddc997cc4c943864c89d63c7
alexclippinger <clippingeralex@gmail.com>
2022-05-12 12:22:57 -0700

    added documentation

--- R/vif_remove.R
+++ R/vif_remove.R
@@ -1,3 +1,12 @@
+#' @title Removes variables with high Variance Inflation Factors (VIF)
+#' @description The purpose of this function is to remove multicollinearity from a dataset. This is achieved through the following steps: 1. Calculate VIF using a linear model of all numeric predictors.
+#'                   2. Finds the highest VIF value. If greater than the input threshold, that variable is removed.
+#'                   3. Calculate VIF using a linear model of predictors, with the variable from the previous step removed.
+#'                   4. Repeat steps 2-3 until all VIF values are below the input threshold.
+#'
+#' @param df Dataframe: Contains numeric response variable and numeric predictors. Non-numeric predictors are removed.
+#' @param response_col String: Name of response column within df. Default: "response".
+#' @param vif_threshold Numeric: Defines the selection threshold. Higher values result in fewer variables removed and more potential for multicollinearity. Default: 5.
 vif_remove <- function(df, response_col = "response", vif_threshold = 5) {
 
   # Select only numeric columns
